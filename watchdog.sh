3d9b3dab310f7b1fc94a207240f63d9581391920
rfuehrer <rene.fuehrer@generali.com>
2017-01-24 08:49:46 +0100

    Update watchdog.sh
    add argument handling, add display handling

--- watchdog.sh
+++ watchdog.sh
@@ -14,24 +14,43 @@ echo "-file.   : different list file"
 echo "-display : echo output to console"
 echo ""
 
+DISPLAY_ECHO=0
+while [[$# -gt 1]]
+	do
+	key="$1"
+
+	case $key in
+	-display)
+		DISPLAY_ECHO=1
+	;;
+	-file)
+		WATCHDOG_FILE=`echo $key | cut -d':' -f 2
+	;;
+	*)	
+	;;
+esac
+
+
+
+
 while read WATCHDOG_PROCESS; do
 	if [[ "$WATCHDOG_PROCESS" != "#"* ]];then
 		# String no comment
 
 		if [[ $WATCHDOG_PROCESS == -* ]]; then
 			WATCHDOG_KILL=`echo -n "$WATCHDOG_PROCESS" | tail -c +2 | sed -e 's/^[[:space:]]*//'`
-			echo -n "Beende Prozess '$WATCHDOG_KILL'..."
+			[[ $DISPLAY_ECHO == 1]] && echo -n "Beende Prozess '$WATCHDOG_KILL'..."
 			sudo pkill -f $WATCHDOG_KILL
-			echo "OK"
+			[[ $DISPLAY_ECHO == 1]] && echo "OK"
 		else
 			if [[ !  -z  $WATCHDOG_PROCESS  ]]; then
-			    	echo -n "Prüfe Prozess '$WATCHDOG_PROCESS'..."
+			    	[[ $DISPLAY_ECHO == 1]] && echo -n "Prüfe Prozess '$WATCHDOG_PROCESS'..."
 				COUNT=`ps -ef | grep "$WATCHDOG_PROCESS" | grep -v "grep" | wc -l`
 				if [ $COUNT -eq 0 ]; then
-					echo "nicht gefunden!"
+					[[ $DISPLAY_ECHO == 1]] && echo "nicht gefunden!"
 					$($WATCHDOG_PROCESS) &
 				else
-					echo "gefunden. OK"
+					[[ $DISPLAY_ECHO == 1]] && echo "gefunden. OK"
 				fi
 			fi
 		fi
