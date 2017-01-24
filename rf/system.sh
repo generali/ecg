3bb283e42b8df971456a1099a7c3812a1782b8fc
rfuehrer <rene.fuehrer@generali.com>
2017-01-24 09:42:54 +0100

    Update system.sh

--- rf/system.sh
+++ rf/system.sh
@@ -25,7 +25,7 @@ esac
 while true
 do
         CPU=`top -d 0.5 -b -n2 | grep "Cpu(s)"|tail -n 1 | awk '{print $2 + $4}'`
-        [[ DISPLAY_ECHO == 1 ]] && echo "cpu: $CPU"
+        [[ $DISPLAY_ECHO == 1 ]] && echo "cpu: $CPU"
 	# Freier Speicher
         MEM_FREE=`free -m | grep "Mem:" | awk '{print $4}'`
 	[[ $DISPLAY_ECHO == 1 ]] && echo "mem free: $MEM_FREE"
@@ -46,20 +46,20 @@ do
 	[[ $DISPLAY_ECHO == 1 ]] && echo "voltage: $RPI_VOLTAGE"
 	# sdcard_free (%)
 	SDCARD_FREE=`df -h | grep mmcb | awk '{print $5}' | cut -d'%' -f 1`
-	[[ $DISPLAY_ECHO == 1 ]] && echo "sd free: SDCARD_FREE"
+	[[ $DISPLAY_ECHO == 1 ]] && echo "sd free: $SDCARD_FREE"
 
 	URL=`cat /home/pi/circonus/rpi_ecg1_url.txt`
 
-#        curl -X PUT --insecure "$URL" --data '{
-#            "'$THIS_SYSTEM.$THIS_SERIAL.RPI.cpu'": "'$CPU'",
-#            "'$THIS_SYSTEM.$THIS_SERIAL.RPI.mem_free'": "'$MEM_FREE'",
-#	    "'$THIS_SYSTEM.$THIS_SERIAL.RPI.eth0_in'": "'$ETH0_IN'",
-#	    "'$THIS_SYSTEM.$THIS_SERIAL.RPI.eth0_out'": "'$ETH0_OUT'",
-#	    "'$THIS_SYSTEM.$THIS_SERIAL.RPI.wlan0_in'": "'$WLAN0_IN'",
-#	    "'$THIS_SYSTEM.$THIS_SERIAL.RPI.wlan0_out'": "'$WLAN0_OUT'",
-#	    "'$THIS_SYSTEM.$THIS_SERIAL.RPI.sdcard_free'": "'$SDCARD_FREE'",
-#	    "'$THIS_SYSTEM.$THIS_SERIAL.RPI.voltage'": "'$RPI_VOLTAGE'"
-#         }'
+        curl -X PUT --insecure "$URL" --data '{
+            "'$THIS_SYSTEM.$THIS_SERIAL.RPI.cpu'": "'$CPU'",
+            "'$THIS_SYSTEM.$THIS_SERIAL.RPI.mem_free'": "'$MEM_FREE'",
+	    "'$THIS_SYSTEM.$THIS_SERIAL.RPI.eth0_in'": "'$ETH0_IN'",
+	    "'$THIS_SYSTEM.$THIS_SERIAL.RPI.eth0_out'": "'$ETH0_OUT'",
+	    "'$THIS_SYSTEM.$THIS_SERIAL.RPI.wlan0_in'": "'$WLAN0_IN'",
+	    "'$THIS_SYSTEM.$THIS_SERIAL.RPI.wlan0_out'": "'$WLAN0_OUT'",
+	    "'$THIS_SYSTEM.$THIS_SERIAL.RPI.sdcard_free'": "'$SDCARD_FREE'",
+	    "'$THIS_SYSTEM.$THIS_SERIAL.RPI.voltage'": "'$RPI_VOLTAGE'"
+         }'
 
 #        curl -X PUT --insecure "$URL" --data '{
 #            "'$THIS_SYSTEM'"."'$THIS_SERIAL.RPI.cpu'": "'$CPU'",
